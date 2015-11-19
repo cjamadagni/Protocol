188c5073f37489ef1ff250d222d891627722cedc
cjamadagni <chirag.jamadagni@gmail.com>
2015-11-19 23:08:04 +0530

    showing relevant fields in the admin page

--- dbms/admin.py
+++ dbms/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from dbms.models import UserProfile,Topic
 from papers.models import Paper
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	fields = ('user','first_name','last_name','email','username','proffesion','cur_university')
+
+
 admin.site.register(Topic)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Paper)
+
+
